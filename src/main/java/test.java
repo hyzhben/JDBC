c9717067a7e3e4b6cefc6d783f1110c7d6375c9a
hyzhben <hyzhben@126.com>
2020-01-02 11:30:14 +0800

    JDBC第一次提交

---
+++ src/main/java/test.java
@@ -0,0 +1,23 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class test {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://193.112.78.4:3306/mysql?characterEncoding=utf-8";
+        String user = "root";
+        String password = "123456";
+        try {
+            //1. 加载驱动
+            Class.forName("com.mysql.jdbc.Driver");
+            //2. 创建连接
+            Connection connection = DriverManager.getConnection(url,user,password);
+            System.out.println(connection);
+            System.out.println("创建连接成功");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
